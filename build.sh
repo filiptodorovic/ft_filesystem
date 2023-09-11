82b8b42cd38ec5194cc87dd5c4136b8bce7e1431
Filip <filip@Filips-MacBook-Air.local>
2023-09-11 21:31:24 +0200

    Created a new makefile

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc fuse.c -o fs `pkg-config fuse --cflags --libs` -DFUSE_USE_VERSION=26
+clang fuse.c -o fs `pkg-config fuse --cflags --libs` -DFUSE_USE_VERSION=26 -D_FILE_OFFSET_BITS=64
